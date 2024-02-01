9ea9c2bf2b326bff64042672de97c6054a11af3b
Koby Fink <finkkoby@icloud.com>
2024-02-01 16:27:06 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+function findMatching(array, string) {
+    let newArray = array.filter(function(item) {
+        return item.toLowerCase() === string.toLowerCase();
+    })
+    return newArray;
+}
+function fuzzyMatch(array, string) {
+    let stringLength = string.length;
+    let newArray = array.filter(function(item) {
+        console.log(item);
+        debugger;
+        let boo = item.slice(0, (stringLength)) === string;
+        console.log(boo);
+        debugger;
+        return boo;
+    });
+    return newArray;
+}
+function matchName(array, string) {
+    let newArray = array.filter(function(object) {
+        return object.name === string;
+    });
+    return newArray;
+}
+
+const array = ["Matthew", "Marcus", "Lucas", "Jonathon"];
+fuzzyMatch(array, "Ma");
